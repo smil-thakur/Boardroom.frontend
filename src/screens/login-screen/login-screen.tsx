b19801ebe3d43f0e9ad5f4a406f53bec56c119b2
smil-thakur <smil.raj.thakur@gmail.com>
2026-04-19 10:38:42 +0000

    refactor: remove password label and forgot password link from login screen

--- src/screens/login-screen/login-screen.tsx
+++ src/screens/login-screen/login-screen.tsx
@@ -127,15 +127,6 @@ const LoginScreen = () => {
                   )}
                 </div>
                 <div className="grid gap-2">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                    <a
-                      href="#"
-                      className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                    >
-                      Forgot your password?
-                    </a>
-                  </div>
                   <Input
                     {...register("password")}
                     id="password"
